ad16d46c98839a47052637e5b79ed1acfb72429c
Taiwo Tom-Ayegunle <taiwotom-ayegunle@Taiwos-MacBook-Pro.local>
2023-07-10 11:34:05 +0100

    commit

--- src/component/header.js
+++ src/component/header.js
@@ -1,49 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
+import { Modal } from "react-bootstrap"; 
 
-function Header() {
-  function signUpForm() {
-    <div>
-      <form>
-        <div className="mb-3 mt-3">
-          <label htmlFor="email" className="form-label">
-            Email:
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            placeholder="Enter email"
-            name="email"
-          ></input>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="pwd" className="form-label">
-            Password:
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="pwd"
-            placeholder="Enter password"
-            name="pwd"
-          ></input>
-        </div>
-        <div className="form-check mb-3">
-          <label className="form-check-label">
-            <input className="form-check-input" type="checkbox" name="remember">
-              {" "}
-              Remember me{" "}
-            </input>
-          </label>
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    </div>;
-  }
+function Header(props) {
+  const [show, setShow] = useState(false);
 
-  return (
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+  
+
+ return (
+<div>
     <div className="header-box">
       <button type="button" className="btn">
         Contact us
@@ -51,12 +18,68 @@ function Header() {
       <button
         type="submit"
         className="btn bg-warning signup-btn"
-        onClick={signUpForm}
+        onClick={handleShow}
       >
         Sign up
       </button>
     </div>
+    <>
+    <div
+className="modal show"
+style={{ display: 'block', position: 'initial' }}
+></div>
+
+<Modal
+    {...props}
+    size="md"
+    aria-labelledby="contained-modal-title-vcenter"
+    centered
+    show={show} onHide={handleClose}><Modal.Header closeButton>
+        <Modal.Title>Sign Up</Modal.Title>
+    </Modal.Header><Modal.Body>
+
+<form>
+<div className="mb-3 mt-3">
+<label htmlFor="email" className="form-label">
+Email:
+</label>
+<input
+type="email"
+className="form-control"
+id="email"
+placeholder="Enter email"
+name="email"
+
+/>
+</div>
+<div className="mb-3">
+<label htmlFor="pwd" className="form-label">
+Password:
+</label>
+<input
+type="password"
+className="form-control"
+id="pwd"
+placeholder="Enter password"
+name="pwd"
+/>
+</div>
+<div className="form-check mb-3">
+<label className="form-check-label">
+<input className="form-check-input" type="checkbox" name="remember" />
+
+</label>
+</div>
+<button type="submit" className="btn btn-primary">
+Submit
+</button>
+</form>
+</Modal.Body>
+</Modal>
+</>
+</div>
   );
+  
 }
 
 export default Header;
